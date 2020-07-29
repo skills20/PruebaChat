4bae059e5721253045f436379af6933b808ab1cf
Daniel González <danigoza@gmail.com>
2020-07-28 21:29:29 -0500

    user can send or receive messages to and from a specific contact

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -11,14 +11,17 @@ export class ChatComponent implements OnInit {
 
   userChat = {
     user: '',
+    contact: '',
     text: ''
   }
 
   myMessages;
+  myOwnMessages = [];
   myContacts;
   eventSendMsg = "send-message";
   eventSelectContact = "select-contact";
   eventChatTyping = "chat-typing";
+  updateMessages = "update-Messages";
   typingChat = "";
 
   constructor(private activated: ActivatedRoute, private webService: WebSocketService) { }
@@ -26,39 +29,65 @@ export class ChatComponent implements OnInit {
   ngOnInit(): void {
     const id = this.activated.snapshot.params.id;
     this.userChat.user = id;
-    // para los mensajes
+    // Cuando recibo un mensaje
     this.webService.listen('text-event').subscribe((data) => {
-      this.typingChat = '';
       this.myMessages = data;
+      this.typingChat = '';
+      this.myOwnMessages = [];
+
+      this.myMessages.forEach(msg => {
+        if ((msg.user === this.userChat.user && msg.contact === this.userChat.contact) || (msg.user === this.userChat.contact && msg.contact === this.userChat.user)) {
+          this.myOwnMessages.push(msg);
+        }
+      });
     })
-    //para los contactos
+
+    //Actualizar Contactos
+    this.webService.listen('update-Messages').subscribe((data) => {
+      this.myMessages = data;
+      this.typingChat = '';
+      this.myOwnMessages = [];
+
+      this.myMessages.forEach(msg => {
+        if ((msg.user === this.userChat.user && msg.contact === this.userChat.contact) || (msg.user === this.userChat.contact && msg.contact === this.userChat.user)) {
+          this.myOwnMessages.push(msg);
+        }
+      });
+    })
+
+    //Cuando un usuario nuevo ingresa
     this.webService.listen('join-event').subscribe((data) => {
       this.myContacts = data;
       for (var i = 0; i < this.myContacts.length; i++) {
         if (this.myContacts[i].name === this.userChat.user) {
-          console.log(this.myContacts[i]);
           this.myContacts.splice(i, 1)
         }
       }
     })
 
+    //Cuando alguien está escribiendo
     this.webService.listen('chat-typing').subscribe((data) => {
       this.typingChat = '';
       this.typingChat += data;
     })
   }
 
+  //Cuando envío un mensaje
   myMessage() {
     this.webService.emit(this.eventSendMsg, this.userChat);
     this.userChat.text = '';
     this.typingChat = '';
   }
 
+  //Cuando alguien estoy está escribiendo
   typing(event: any) {
     this.webService.emit(this.eventChatTyping, this.userChat.user);
   }
 
-  selectContact(value: String) {
-    this.webService.emit(this.eventSelectContact, this.userChat);
+  //Cuando Selecciono un Contacto
+  setVal(i) {
+    this.userChat.contact = this.myContacts[i].name;
+    this.webService.emit(this.updateMessages, this.userChat);
   }
 }
+
